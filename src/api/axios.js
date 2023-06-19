6707e21acfc4ae40bdd519fc9b7f960586b26ce1
Sera Han <hsr0782@gmail.com>
2023-06-19 15:07:37 +0900

    api key 수정

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   params: {
-    api_key: "df8768246452a99e3e597cc945384041",
+    api_key: process.env.REACT_APP_MOVIE_DB_API_KEY
     language: "ko-KR",
   },
 });
